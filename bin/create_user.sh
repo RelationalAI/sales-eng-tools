ccba17ed9f498ba148c8f20afdd839c4f3c62479
Steve Herskovitz <steve.herskovitz@relational.ai>
2023-03-07 10:06:05 -0800

    add script to create a new user

---
+++ bin/create_user.sh
@@ -0,0 +1,16 @@
+# create specified user
+# if no role specified, make a "regular" (not-Admin) user
+
+source ./envcli.sh
+pro=$RAI_CLI_PROFILE
+
+[[ $# < 1 ]] && echo "usage: $0 email [user|admin]" && exit 13
+email=$1
+role=$2
+[[ "$role" == "" ]] && role="user"
+if [[ "$role" =~ ^(user|admin)$ ]]; then
+    echo "creating user '$email' with role '$role'"
+    rai --profile $pro create-user $email --role $role
+else
+    echo "usage: role must be 'user' or 'admin'" && exit 13
+fi
