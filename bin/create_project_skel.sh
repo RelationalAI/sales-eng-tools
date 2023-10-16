f65d2083229a7c746aa39c4641d1f91a8f7c4a01
Steve Herskovitz <steve.herskovitz@relational.ai>
2023-10-15 23:56:05 -0700

    add viz directory

--- bin/create_project_skel.sh
+++ bin/create_project_skel.sh
@@ -2,12 +2,12 @@
 [ $# != 2 ] && echo "usage: $0 <base> <appname>" && exit 13
 
 # run this from the customer directory
-# for example: 
+# for example:
 # cd ~/src/rai/sales-engineering/<customer>
 # create_project_skel.sh poc1 tearsheet
 
 BASE=$1
 APPNAME=$2
 
-mkdir -pv $BASE/$APPNAME/{model,util,constraint}
+mkdir -pv $BASE/$APPNAME/{model,util,viz,constraint}
 mkdir -pv $BASE/{bin,orm,query,update,test,test_log,sample_data,snowflake}
